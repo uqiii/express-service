a6dbc6588010a931c146fd74a22b8630fac87dd3
Uqi <uqi.alfa@gmail.com>
2023-12-16 13:47:40 +0700

    handle unknown error

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,7 +3,14 @@ const errorHandler = (err, req, res, next) => {
     return next();
   }
 
-  console.log('===error : ', err);
+  if (!err.status) {
+    res.status(500).json({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: err.message
+    });
+    next();
+  }
+
   res.status(err.status).json({
     code: err.code,
     message: err.message
